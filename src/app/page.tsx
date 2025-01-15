260eaab8da030c2120bcd11568aff4ad6b561e83
Ari Miller <ari.miller@constantcontact.com>
2025-01-15 11:37:30 -0500

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,21 @@
-"use client";
+import { headers } from "next/headers";
 
-import { useParameterStore } from "@statsig/react-bindings";
+export default async function Home() {
+  const headersList = await headers();
+  const userAgent = headersList.get("user-agent");
 
-export default function Home() {
-  const store = useParameterStore("param_store_debug_test");
-  const cards = store.get("cards", ["default value"]);
-  console.log({ store });
+  console.log({ "-------- DEBUG --------": { userAgent } });
+
+  const todo = await fetch("https://jsonplaceholder.typicode.com/todos/1").then(
+    (response) => response.json()
+  );
+
+  console.log({ "-------- DEBUG --------": { todo } });
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <pre>{JSON.stringify(cards, null, 2)}</pre>
+        <pre>{JSON.stringify(userAgent, null, 2)}</pre>
       </main>
     </div>
   );
